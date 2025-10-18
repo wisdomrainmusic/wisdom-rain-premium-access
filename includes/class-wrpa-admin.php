f5359af62bd056674b20d4ba33e1620a08a8365c
wisdomrainmusic <alisverissepetiniz4601@gmail.com>
2025-10-18 09:16:51 +0300

    Add WRPA_Admin menu with Hello WRPA 2.0 dashboard test

---
+++ includes/class-wrpa-admin.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * WRPA - Admin Module
+ * @package WisdomRain\PremiumAccess
+ */
+
+namespace WRPA;
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class WRPA_Admin {
+    public static function init() {
+        add_action( 'admin_menu', [ __CLASS__, 'register_menu' ] );
+    }
+
+    public static function register_menu() {
+        add_menu_page(
+            'WRPA Dashboard',         // Sayfa başlığı
+            'WRPA',                   // Menü adı
+            'manage_options',         // Yetki
+            'wrpa-dashboard',         // Slug
+            [ __CLASS__, 'dashboard_page' ], // Callback
+            'dashicons-star-filled',  // İkon
+            2                         // Sıra
+        );
+    }
+
+    public static function dashboard_page() {
+        echo '<div class="wrap"><h1>Hello WRPA 2.0</h1><p>Dashboard başarıyla yüklendi.</p></div>';
+    }
+}
